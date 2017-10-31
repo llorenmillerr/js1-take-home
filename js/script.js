70a036e9433fadeae2b403522efd5354533a97d2
Loren Miller <llorenmillerr@gmail.comm>
2017-10-30 21:05:17 -0700

    added basic index.html framework / links, and logMessage() on btn click

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var name = "Loren"
+var orgName = "Geekwise"
+var className = "Javascript 1"
+
+function logMessage() {
+  //logs message on btn tag click
+  var message = `Hi, my name is ${name} and I am stoked to be here at ${orgName} taking ${className}. I am hoping to learn more about object oriented programming and how to implement it on a professional scale :)`
+  console.log(message);
+}
